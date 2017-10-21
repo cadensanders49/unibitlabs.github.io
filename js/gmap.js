57bc8f041d3b2b21eca7557a3485d827b5f1b203
Caden Sanders <cadensanders49@gmail.com>
2017-10-21 17:57:30 -0500

    Multiple Fixes
    -New home page video
    -Social Links fixed
    -Libraries updated
    -Styles updated
    -Button allignment
    -Other updates
    -Still under construction

--- js/gmap.js
+++ js/gmap.js
@@ -24,7 +24,7 @@
 				center: folsom,
                 // This is where you would paste any style found on Snazzy Maps.
 				styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
-  
+
 			  };
 
 			  map = new google.maps.Map(document.getElementById('google-map'),
@@ -49,5 +49,5 @@
 			}
 
 			google.maps.event.addDomListener(window, 'load', initialize);
-	
+
 })(jQuery);
